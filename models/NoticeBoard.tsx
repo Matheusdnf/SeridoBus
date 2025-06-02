9000abd594eb0ca8ef6b2a994b0f018ca1b80275
matheusdnf <matheusdiniz870@gmail.com>
2025-06-02 17:56:27 -0300

    feat: model dos objetos do banco de dados (quadro de aviso)

---
+++ models/NoticeBoard.tsx
@@ -0,0 +1,8 @@
+  // Avisos postados por usuários
+  export class NoticeBoard {
+    constructor(
+      public id: number,
+      public notice: string,
+      public user_id: number | null
+    ) {}
+  }
