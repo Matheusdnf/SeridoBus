583c77d004077cee115d570c32eae829fe67a5b9
matheusdnf <matheusdiniz870@gmail.com>
2025-06-02 17:55:33 -0300

    feat: model dos objetos do banco de dados ônibus

---
+++ models/Bus.tsx
@@ -0,0 +1,9 @@
+  // Ônibus cadastrado
+  export class Bus {
+    constructor(
+      public id: number,
+      public name: string,
+      public maxCapacity: number,
+      public color?: string
+    ) {}
+  }
