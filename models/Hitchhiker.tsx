61d2faff481a2e10f74176abaab07be64618493f
matheusdnf <matheusdiniz870@gmail.com>
2025-06-02 17:55:41 -0300

    feat: model dos objetos do banco de dados lista

---
+++ models/Hitchhiker.tsx
@@ -0,0 +1,9 @@
+  // Caroneiro (sem registro no sistema)
+  export class Hitchhiker {
+    constructor(
+      public id: number,
+      public name: string,
+      public destination: string
+    ) {}
+  }
+  
