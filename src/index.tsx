12163e9734fa18f0dacb84b5625534fe45f9832b
Matheusdnf <matheusdiniz870@gmail.com>
2025-06-07 19:50:13 -0300

    feat: novas telas

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,9 @@ import HomeScreen from "./screens/HomeScreen";
 import ListScreen from "./screens/ListScreen";
 import LoginPage from "./screens/auth/login";
 import Register from "./screens/auth/register";
-import DefaultScreen from "./screens/DefaultScreen"
+import DefaultScreen from "./screens/DefaultScreen";
+import New_Password from "./screens/auth/reset_password/new_password";
+import Request_New_Password from "./screens/auth/reset_password/request_new_password";
 import "./styles/global.css";
 const Stack = createNativeStackNavigator();
 
@@ -17,7 +19,7 @@ export default function App() {
         {/* <Stack.Screen name="List" component={ListScreen} /> */}
         {/* <Stack.Screen name="Testando" component={Register} /> */}
         <Stack.Screen name="Login" component={Register} />
-        <Stack.Screen name="Listagem" component={DefaultScreen} />
+        <Stack.Screen name="Listagem" component={Register} />
       </Stack.Navigator>
     </NavigationContainer>
   );
