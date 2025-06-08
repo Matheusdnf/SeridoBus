2be12ff78f8e41fde6c682f1425f4f94ce5bc110
MarlisonSilva <marlisonsoaresjp@gmail.com>
2025-06-08 11:44:18 -0300

    fix: correção de navegação entre telas #11

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ListScreen from "./screens/ListScreen";
 import LoginScreen from "./screens/auth/login";
 import RegisterScreen from "./screens/auth/register";
-import DefaultScreen from "./screens/DefaultScreen";
 import New_Password from "./screens/auth/reset_password/new_password";
 import Request_New_Password from "./screens/auth/reset_password/request_new_password";
 import "./styles/global.css";
@@ -17,7 +16,6 @@ export default function App() {
         <Stack.Screen name="List" component={ListScreen} options={{ headerShown: false }}/>
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Default" component={DefaultScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
