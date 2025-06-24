6fd08de966987ca423f40b27e89684d6edfee686
matheusdnf <matheusdiniz870@gmail.com>
2025-06-24 14:29:15 -0300

    fix: instalação e organização das dependencias #24

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
-// jest.config.js
 module.exports = {
-  preset: "react-native",
-  setupFilesAfterEnv: ["./jest.setup.js"],
+  preset: 'react-native',
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!@react-native-async-storage/async-storage|other-lib-to-mock)/',
+  ],
 };
--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,13 @@
 // jest.setup.js
+import '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
 import "react-native-url-polyfill/auto"; // Polyfill necessário para o Supabase
 require("dotenv").config({
   path: ".env",
 });
+jest.mock('@react-native-async-storage/async-storage', () => ({
+  setItem: jest.fn(),
+  getItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+}));
