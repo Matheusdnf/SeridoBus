d758fbeab22fe09dd0145ad3a30c16a33f635e72
Matheusdnf <matheusdiniz870@gmail.com>
2025-07-01 17:52:28 -0300

    feat: modificação na classe de usuário

--- src/models/User.tsx
+++ src/models/User.tsx
@@ -1,11 +1,11 @@
-  // Usuário do sistema
-  export class User {
-    constructor(
-      public id: number,
-      public code: number,
-      public name: string,
-      public email: string,
-      public cellphone?: string,
-      public pin?: string
-    ) {}
-  }
+// Usuário do sistema
+export class User {
+  constructor(
+    public id: number,
+    public code: number,
+    public name: string,
+    public email: string,
+    public pin: string,
+    public cellphone?: string
+  ) {}
+}
