ebe8b1f1359c1fb2c829ede5ca6008d8d125bce3
Matheusdnf <matheusdiniz870@gmail.com>
2025-07-02 17:01:04 -0300

    feat: modelagem e crud de companhia

---
+++ src/models/Company.tsx
@@ -0,0 +1,3 @@
+export class Company {
+  constructor(public id: number, public name: string) {}
+}
---
+++ src/services/services_company.tsx
@@ -0,0 +1,52 @@
+import { supabase } from "../database/supabase";
+import { Company } from "../models/Company";
+
+export default class CompanyService {
+  static async RegisterCompany(Company: Company) {
+    const { data, error } = await supabase
+      .from("company")
+      .insert({
+        name: Company.name,
+      })
+      .select();
+    if (error) throw error;
+    return data[0];
+  }
+
+  static async EditCompany(Company: Company) {
+    const { data, error } = await supabase
+      .from("company")
+      .update({
+        name: Company.name,
+      })
+      .eq("id", Company.id)
+      .select();
+    if (error) throw error;
+    return data[0];
+  }
+
+  static async DeleteCompany(Company: Company) {
+    const { data, error } = await supabase
+      .from("company")
+      .delete()
+      .eq("id", Company.id);
+    if (error) throw error;
+  }
+  static async SearchCompany(Company: Company, name: string) {
+    const { data, error } = await supabase
+      .from("company")
+      .select("*")
+      .eq("name", name);
+    if (error) throw error;
+    return data[0];
+  }
+
+  static async ListCompany() {
+    const { data, error } = await supabase
+      .from("company")
+      .select("*")
+      .order("name", { ascending: true });
+    if (error) throw error;
+    return data as Company[];
+  }
+}
