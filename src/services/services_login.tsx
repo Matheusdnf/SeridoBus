a16b5b6af1d77707bf30eb8b07b0d00bdbfcf911
Matheusdnf <matheusdiniz870@gmail.com>
2025-07-05 17:25:56 -0300

    feat: adicionando as funções de redefinir senha e login

--- src/services/services_login.tsx
+++ src/services/services_login.tsx
@@ -6,21 +6,92 @@ export default class AuthService {
     const { data, error } = await supabase.auth.signUp({
       email: user.email,
       password: user.pin,
+      phone: user.cellphone,
     });
+
     if (error) throw error.message;
+
+    const userid = data?.user?.id;
+
+    if (userid) {
+      const { error: insertError } = await supabase
+        .from("user_profile")
+        .insert({
+          id: userid,
+          name: user.name,
+          adm_company: user.adm_company ?? false,
+          create_company: user.create_company ?? false,
+          associate: user.associate ?? false,
+          company_id: user.company_id ?? null,
+          cellphone: user.cellphone,
+        });
+
+      if (insertError) throw insertError.message;
+    }
+
     return data;
   }
+
   static async SignInWithEmail(user: User) {
     const { data, error } = await supabase.auth.signInWithPassword({
       email: user.email,
       password: user.pin,
     });
+
+    if (error) throw error.message;
+
+    const userId = data?.user?.id;
+
+    let profile = null;
+
+    if (userId) {
+      const { data: profileData, error: profileError } = await supabase
+        .from("user_profile")
+        .select("*")
+        .eq("id", userId)
+        .single();
+
+      if (profileError) throw profileError.message;
+
+      profile = profileData;
+    }
+
+    return { auth: data, profile }; // retorna login + perfil
+  }
+  static async SendEmailRedefinitionPassword(email: string) {
+    const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
+      // configurar no supabase a rota para a tela de atualização de senha
+      redirectTo: "https://example.com/update-password",
+    });
+    if (error) {
+      console.error(error.message);
+    } else {
+      // informar que email foi enviado
+      console.log("Email de recuperação enviado!");
+    }
+  }
+  // gerar token para redefinição de senha
+  static async setSessionFromToken(token: string) {
+    const { error } = await supabase.auth.setSession({
+      access_token: token,
+      refresh_token: "",
+    });
+
+    if (error) throw error.message;
+  }
+
+  //atualizar para nova senha
+  static async updatePassword(newPassword: string) {
+    const { data, error } = await supabase.auth.updateUser({
+      password: newPassword,
+    });
+
     if (error) throw error.message;
     return data;
   }
-  //   static async GetLogin(user: User) {
-  //     const {
-  //       data: { User },
-  //     } = await supabase.auth.getUser();
-  //   }
+  // passa rota que ele precisa ir, onde ele se redirecionar
+  static async signOut(user: User, navigation: any) {
+    const { error } = await supabase.auth.signOut({ scope: "local" });
+    navigation.replace(navigation);
+  }
 }
